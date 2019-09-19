8adb592fcc4940f6e4047bcf53f31c8ae10c4f93
c03rcion <areeves9@icloud.com>
2019-09-18 21:37:18 -0700

    created 30 days vehc incidents function, route with params, view function

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+import datetime
+from sqlalchemy import cast
+from geoalchemy2 import Geography
+import geoalchemy2.functions as geoalchemy2
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 
 app = Flask(
     __name__,
@@ -13,12 +18,29 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-import models
+from models import Incident
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+def get_thirty_days_vehicle_incidents(lng, lat, radius):
+    d = datetime.date.today() - datetime.timedelta(days=100)
+    incidents = Incident.query.filter(Incident.incident_datetime.between(
+        d, datetime.date.today())).filter(geoalchemy2.ST_DWithin(
+            Incident.point, 
+            (cast(geoalchemy2.ST_MakePoint(lng, lat), Geography)), radius), 
+            Incident.incident_subcategory.like('%Vehicle%')).all()
+    return jsonify(
+        results=len(incidents), 
+        incidents=[incident.serialize for incident in incidents])
+
+
+@app.route(
+    # use werkzeug bulit-in float converter (signed=True)
+    '/vehicles/<float(signed=True):longitude>/<float(signed=True):latitude>/<int:radius>',
+    methods=['GET']
+    )
+def vehicle_incidents(longitude, latitude, radius):
+    if request.method == 'GET':
+        return get_thirty_days_vehicle_incidents(longitude, latitude, radius)
 
 
 if __name__ == '__main__':
