30bf192217f90c2b1c387af9e326a2dfc04f9fba
c03rcion <areeves9@icloud.com>
2019-09-20 12:17:19 -0700

    added pagination to response

--- app.py
+++ app.py
@@ -21,18 +21,39 @@ migrate = Migrate(app, db)
 from models import Incident
 
 
-def get_thirty_days_vehicle_incidents(lng, lat, radius):
+def get_timedelta():
     d = datetime.date.today() - datetime.timedelta(days=100)
-    incidents = Incident.query.filter(Incident.incident_datetime.between(
-        d, datetime.date.today())).filter(geoalchemy2.ST_DWithin(
-            Incident.point, 
-            (cast(geoalchemy2.ST_MakePoint(lng, lat), Geography)), radius), 
-            Incident.incident_subcategory.like('%Vehicle%')).all()
+    return d
+
+
+def get_thirty_days_vehicle_incidents(lng, lat, radius, page, per_page):
+    filtered_incidents = (Incident.query.filter(
+        Incident.incident_datetime.between(
+            get_timedelta(), datetime.date.today())
+        ).filter(
+            geoalchemy2.ST_DWithin(
+                Incident.point, (
+                    cast(
+                        geoalchemy2.ST_MakePoint(lng, lat), Geography
+                        )
+                    ), radius
+                ), Incident.incident_subcategory.like('%Vehicle%')))
+    
+    vehicle_incidents = filtered_incidents.paginate(page, per_page, False).items
+    pages = filtered_incidents.paginate(page, per_page, False).pages
+    
     return jsonify(
-        results=len(incidents), 
-        incidents=[incident.serialize for incident in incidents])
+        meta={
+            "total_incidents": filtered_incidents.count(),
+            "current_page": request.args.get('page', type=int),
+            "per_page": request.args.get('limit', type=int),
+            "pages": pages,
+        },
+        vehicle_incidents=[incident.serialize for incident in vehicle_incidents],
+        )
 
 
+# get vehicle related incidents for previous 30 days
 @app.route(
     # use werkzeug bulit-in float converter (signed=True)
     '/vehicles/<float(signed=True):longitude>/<float(signed=True):latitude>/<int:radius>',
@@ -40,7 +61,10 @@ def get_thirty_days_vehicle_incidents(lng, lat, radius):
     )
 def vehicle_incidents(longitude, latitude, radius):
     if request.method == 'GET':
-        return get_thirty_days_vehicle_incidents(longitude, latitude, radius)
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('limit', 10, type=int)
+        return get_thirty_days_vehicle_incidents(longitude, latitude, radius, page, per_page)
+
 
 if __name__ == '__main__':
     app.run()
